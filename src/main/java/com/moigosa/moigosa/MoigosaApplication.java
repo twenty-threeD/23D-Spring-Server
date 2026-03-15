e4b1f9ee8c4437fd0db0ff3e7e51568ac5002727
김경윤 <l982930009@gmail.com>
2026-03-16 08:06:28 +0900

    feat: add base set

---
+++ src/main/java/com/moigosa/moigosa/MoigosaApplication.java
@@ -0,0 +1,13 @@
+package com.moigosa.moigosa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoigosaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoigosaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moigosa/moigosa/MoigosaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moigosa.moigosa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoigosaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
